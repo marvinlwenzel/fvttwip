bfe46fc15475731df8860434d3837a3262cc645b
Marvin Lukas Wenzel <mlw@mlw.wtf>
2024-11-01 03:29:31 +0100

    more

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/marvinlwenzel/fvttwip
 
 go 1.22.7
 
-require github.com/caddyserver/caddy/v2 v2.8.4
+require (
+	github.com/caddyserver/caddy/v2 v2.8.4
+	golang.org/x/crypto v0.23.0
+)
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
@@ -94,7 +97,6 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	go.uber.org/zap/exp v0.2.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/crypto/x509roots/fallback v0.0.0-20240507223354-67b13616a595 // indirect
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
 	golang.org/x/mod v0.17.0 // indirect
--- my.go
+++ my.go
@@ -1,11 +1,14 @@
-package mymodule
+package main
 
 import (
+	"crypto/rand"
+	"crypto/sha512"
 	"encoding/hex"
 	"fmt"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp/caddyauth"
+	"golang.org/x/crypto/pbkdf2"
 )
 
 func init() {
@@ -16,26 +19,49 @@ func init() {
 type FvttCrypter struct{}
 
 func (FvttCrypter) Hash(plaintext []byte) ([]byte, error) {
-	fmt.Println("MLW Hash")
-	fmt.Println("MLW Hash")
-	fmt.Println("MLW Hash")
-	fmt.Println("MLW Hash")
-	fmt.Println(plaintext)
-	hexString := "4c6561726e20476f21"
-	return hex.DecodeString(hexString)
+	saltRandomness := make([]byte, 32)
+	// [25 27 ... 40] (len=32)
+	_, err := rand.Read(saltRandomness)
+	if err != nil {
+		return nil, err
+	}
+
+	// 191bebf98a1111dc092dfb2de19f46a1a59df14db8e234654b68452562b28a28
+	saltHexString := hex.EncodeToString(saltRandomness)
+
+	// [49 57 49 98 ... 50 56] (len=64)
+	saltBytes := []byte(saltHexString)
+
+	hash := pbkdf2.Key(plaintext, saltBytes, 1000, 64, sha512.New)
+	hexHashBytes := []byte(hex.EncodeToString(hash))
+
+	arr := make([]byte, 201)
+	n := 0
+	copy(arr[n:], []byte("$fvtt1a$"))
+	n = 8
+	copy(arr[n:], saltBytes)
+	n += len(saltBytes)
+	arr[n] = '$'
+	n += 1
+	copy(arr[n:], hexHashBytes)
+	n += len(hexHashBytes)
+	return arr, nil
 }
 func (FvttCrypter) FakeHash() []byte {
-	hexString := "4c6561726e20476f21"
-	h, _ := hex.DecodeString(hexString)
-	return h
+	fake := []byte("$fvtt1a$191bebf98a1111dc092dfb2de19f46a1a59df14db8e234654b68452562b28a28$c27139bff8c68a5f9f8228b7f00b41db5fc001841497d99d5422aa505f1a83c1ed37bb159857bbb8bc44905f47d3873419e26e0c3ce37d6c8b1544b645f07e1b")
+	return fake
 }
 func (FvttCrypter) Compare(hashed, plaintext []byte) (bool, error) {
 	fmt.Println("MLW Compare")
 	fmt.Println("MLW Compare")
 	fmt.Println("MLW Compare")
 	fmt.Println("MLW Compare")
+	fmt.Println("hashed")
 	fmt.Println(hashed)
+	fmt.Println(string(hashed))
+	fmt.Println("plaintext")
 	fmt.Println(plaintext)
+	fmt.Println(string(plaintext))
 	// err := new(FvttCrypter)
 	// if err != nil {
 	// 	return false, err
@@ -45,12 +71,8 @@ func (FvttCrypter) Compare(hashed, plaintext []byte) (bool, error) {
 
 // CaddyModule returns the Caddy module information.
 func (FvttCrypter) CaddyModule() caddy.ModuleInfo {
-	fmt.Println("MLW caddy.ModuleInfo")
-	fmt.Println("MLW caddy.ModuleInfo")
-	fmt.Println("MLW caddy.ModuleInfo")
-	fmt.Println("MLW caddy.ModuleInfo")
 	return caddy.ModuleInfo{
-		ID:  "http.authentication.hashes.fvttcryp",
+		ID:  "http.authentication.hashes.fvttcrypt",
 		New: func() caddy.Module { return new(FvttCrypter) },
 	}
 }
